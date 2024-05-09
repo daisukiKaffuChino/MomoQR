08e8cdd91a9a72fbb5c7d333eec963826c173dc4
daisukiKaffuChino <483004683@qq.com>
2024-05-09 18:20:25 +0800

    rollback error

--- MomoQR/app/src/main/java/github/daisukiKaffuChino/MomoQR/MAppGlideModule.java
+++
@@ -1,8 +0,0 @@
-package github.daisukiKaffuChino.MomoQR;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public class MAppGlideModule extends AppGlideModule {
-}
