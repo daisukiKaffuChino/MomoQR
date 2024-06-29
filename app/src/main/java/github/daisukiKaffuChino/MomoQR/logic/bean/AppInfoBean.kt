b27bf576ef99576dd0e01f6143c578fb32305e1c
KaffuChino <47238156+daisukiKaffuChino@users.noreply.github.com>
2024-06-29 21:51:50 +0800

    convert code

---
+++ app/src/main/java/github/daisukiKaffuChino/MomoQR/logic/bean/AppInfoBean.kt
@@ -0,0 +1,9 @@
+package github.daisukiKaffuChino.MomoQR.logic.bean
+
+import android.graphics.drawable.Drawable
+
+class AppInfoBean {
+    var icon: Drawable? = null
+    var appName: String? = null
+    var appPackageName: String? = null
+}
